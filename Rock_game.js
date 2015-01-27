487328b7586913e902ae5741d1e6715b92fbc88f
romanarellano <romanarellano@yahoo.com>
2015-01-26 16:26:26 -1000

    created try again button

--- Rock_game.js
+++ Rock_game.js
@@ -1,32 +1,26 @@
 window.onload = function() {
+  
+
+
   rockpaper();
 };
+ 
+img.onload=function(){
 
-
-function user1(){
-
-
-
-}
-
-function user2(){
-
-
-
-}
-
-
-
+        image.appendChild(img);
+      };
 
 function rockpaper(){
 
   var chosen;
 
+  var img = new Image();
+
   var elements = ['rock','paper','fireball'];
 
   var You = prompt('Type: \n rock,paper, or fireball');
 
-  var choose=('You typed ' + You);
+  var Yours=('You typed ' + You);
 
 
   if(elements.indexOf(You)==-1){
@@ -38,26 +32,36 @@ function rockpaper(){
 
   var Computer = elements[Math.floor(Math.random()*3)];
 
+  var image =document.getElementById('yourPick');
+
   switch(You){
 
 
     case "rock": 
 
-    if(Computer === 'rock')chosen=('Tie game');
+    if(Computer === 'rock'){
+      chosen=('Tie game');
 
-    else if(Computer === 'paper')chosen=('You lose!' +'\n' +'Computer chose ' + Computer );
-
-    else chosen=('You win' + '\n' + 'Computer chose ' + elements[2] );
+    }else if(Computer === 'paper'){
+      chosen=('You lose!' +'\n' +'Computer chose ' + Computer );
+      img.src='https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ0iHuXcwu9PCSTCfXER-WtYCSH8nZ_gDOK88nltRtybjSwpiKx8dz4F9s';
+      
+     
+    }else chosen=('You win' + '\n' + 'Computer chose ' + elements[2] );
 
     break;
     
     case "paper": 
 
-    if(Computer === 'paper')chosen=('Tie game');
+    if(Computer === 'paper'){
+      chosen=('Tie game');
 
-    else if(Computer === 'fireball')chosen=('You lose!' +'\n' +'Computer chose ' + Computer );
+    }else if(Computer === 'fireball'){
+      chosen=('You lose!' +'\n' +'Computer chose ' + Computer );
+      img.src='https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ0iHuXcwu9PCSTCfXER-WtYCSH8nZ_gDOK88nltRtybjSwpiKx8dz4F9s';
+      image.appendChild(img);
 
-    else chosen=('You win' + '\n' + 'Computer chose ' +elements[0]);
+    }else chosen=('You win' + '\n' + 'Computer chose ' +elements[0]);
 
     break;
 
@@ -68,15 +72,19 @@ function rockpaper(){
       chosen=('Tie game');
 
     }else if(Computer === 'rock'){
-      chosen=('You lose!' +'\n' +'Computer chose ' + elements[1] );
+      chosen=("You lose!" + "</br>" +"Computer chose " + elements[1] );
+      img.src='https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ0iHuXcwu9PCSTCfXER-WtYCSH8nZ_gDOK88nltRtybjSwpiKx8dz4F9s';
+      image.appendChild(img);
 
     }else {
-      chosen=('You win' + '\n' + 'Computer chose ' +elements[1]);
+      chosen=("You win" + "</br>" + "Computer chose " +elements[1]);
     }
     }
-    document.getElementById('test').innerHTML=choose;
+    
+    image.innerHTML=Yours;
+
     document.getElementById('runGame').innerHTML=chosen;
-    //return chosen + "\n"+ "Thank you for playing!";
+    
 }
 
 
